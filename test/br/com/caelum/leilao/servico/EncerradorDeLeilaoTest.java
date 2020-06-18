9e57e08bbb6ba57c435820379aff446b6f880fd6
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-06-17 22:21:09 -0300

    Adicionado teste unitario para leiloes que nao devem ser encerrados.

--- test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
+++ test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
@@ -1,5 +1,11 @@
 package br.com.caelum.leilao.servico;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import java.util.Arrays;
 import java.util.Calendar;
 
@@ -8,11 +14,6 @@ import org.junit.Test;
 import br.com.caelum.leilao.builder.CriadorDeLeilao;
 import br.com.caelum.leilao.dominio.Leilao;
 import br.com.caelum.leilao.infra.dao.LeilaoDao;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class EncerradorDeLeilaoTest {
 	
@@ -35,6 +36,25 @@ public class EncerradorDeLeilaoTest {
         assertEquals(2, encerrador.getTotalEncerrados());
         assertTrue(leilao1.isEncerrado());
         assertTrue(leilao2.isEncerrado());
+	}
+	
+	@Test
+	public void naoDeveEncerrarLeiloesQueComecaramMenosDeUmaSemanaAtras() {
+		Calendar ontem = Calendar.getInstance();
+		ontem.add(Calendar.DATE, -1);
+
+		Leilao leilao1 = new CriadorDeLeilao().para("TV de plasma").naData(ontem).constroi();
+		Leilao leilao2 = new CriadorDeLeilao().para("Geladeira").naData(ontem).constroi();
+		
+		LeilaoDao daoFalso = mock(LeilaoDao.class);
+		when(daoFalso.correntes()).thenReturn(Arrays.asList(leilao1, leilao2));
+		
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(daoFalso);
+        encerrador.encerra();
+        
+        assertEquals(0, encerrador.getTotalEncerrados());
+        assertFalse(leilao1.isEncerrado());
+        assertFalse(leilao2.isEncerrado());
 	}
 
 }
