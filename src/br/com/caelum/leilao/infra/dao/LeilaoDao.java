5904219d3e832532c71f4c490a63a0cd613f06df
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-06-17 21:53:27 -0300

    Alterada senha do BD.

--- src/br/com/caelum/leilao/infra/dao/LeilaoDao.java
+++ src/br/com/caelum/leilao/infra/dao/LeilaoDao.java
@@ -22,7 +22,7 @@ public class LeilaoDao {
 	public LeilaoDao() {
 		try {
 			this.conexao = DriverManager.getConnection(
-					"jdbc:mysql://localhost/mocks", "root", "");
+					"jdbc:mysql://localhost/mocks", "root", "root1234");
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
