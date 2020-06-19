1c16d1472f4f2b1545301146ede19ffdfe8eb385
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-06-18 22:49:37 -0300

    Criado teste para geracao de pagamento que usa ArgumentCaptor.

---
+++ test/br/com/caelum/leilao/servico/GeradorDePagamentoTest.java
@@ -0,0 +1,46 @@
+package br.com.caelum.leilao.servico;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import br.com.caelum.leilao.builder.CriadorDeLeilao;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.dominio.Pagamento;
+import br.com.caelum.leilao.dominio.Usuario;
+import br.com.caelum.leilao.infra.dao.RepositorioDeLeiloes;
+import br.com.caelum.leilao.infra.dao.RepositorioDePagamentos;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+public class GeradorDePagamentoTest {
+
+	@Test
+    public void deveGerarPagamentoParaUmLeilaoEncerrado() {
+
+        RepositorioDeLeiloes leiloes = mock(RepositorioDeLeiloes.class);
+        RepositorioDePagamentos pagamentos = mock(RepositorioDePagamentos.class);
+        Avaliador avaliador = mock(Avaliador.class);
+
+        Leilao leilao = new CriadorDeLeilao()
+            .para("Playstation")
+            .lance(new Usuario("José da Silva"), 2000.0)
+            .lance(new Usuario("Maria Pereira"), 2500.0)
+            .constroi();
+
+        when(leiloes.encerrados()).thenReturn(Arrays.asList(leilao));
+        when(avaliador.getMaiorLance()).thenReturn(2500.0);
+
+        GeradorDePagamento gerador = 
+            new GeradorDePagamento(leiloes, pagamentos, avaliador);
+        gerador.gera();
+
+        ArgumentCaptor<Pagamento> argumento = ArgumentCaptor.forClass(Pagamento.class);
+        verify(pagamentos).salva(argumento.capture());
+        Pagamento pagamentoGerado = argumento.getValue();
+        assertEquals(2500.0, pagamentoGerado.getValor(), 0.00001);
+    }
+	
+}
