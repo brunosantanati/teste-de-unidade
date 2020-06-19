f56992fb75eb72d7cabf4739c7ad265eb61c79f9
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-06-18 22:05:18 -0300

    Teste que assegura que a execucao continua em caso de falha de e-mail.

--- test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
+++ test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
@@ -160,5 +160,30 @@ public class EncerradorDeLeilaoTest {
 //        verify(daoFalso, times(1)).atualiza(leilao2);
 //        verify(carteiroFalso, times(1)).envia(leilao2);
         
+    }
+	
+	@Test
+    public void deveContinuarAExecucaoMesmoQuandoEnviadorDeEmaillFalha() {
+        Calendar antiga = Calendar.getInstance();
+        antiga.set(1999, 1, 20);
+
+        Leilao leilao1 = new CriadorDeLeilao().para("TV de plasma")
+            .naData(antiga).constroi();
+        Leilao leilao2 = new CriadorDeLeilao().para("Geladeira")
+            .naData(antiga).constroi();
+
+        RepositorioDeLeiloes daoFalso = mock(RepositorioDeLeiloes.class);
+        when(daoFalso.correntes()).thenReturn(Arrays.asList(leilao1, leilao2));
+
+        Carteiro carteiroFalso = mock(Carteiro.class);
+        doThrow(new RuntimeException()).when(carteiroFalso).envia(leilao1);
+
+        EncerradorDeLeilao encerrador =
+            new EncerradorDeLeilao(daoFalso, carteiroFalso);
+
+        encerrador.encerra();
+
+        verify(daoFalso).atualiza(leilao2);
+        verify(carteiroFalso).envia(leilao2);
     }	
 }
