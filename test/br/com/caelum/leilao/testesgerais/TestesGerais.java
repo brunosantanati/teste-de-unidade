ce57040db3200e18a57e738e5599edb174a2bf3d
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-06-29 16:01:43 -0300

    Exemplo usando anyString().

--- test/br/com/caelum/leilao/testesgerais/TestesGerais.java
+++ test/br/com/caelum/leilao/testesgerais/TestesGerais.java
@@ -3,6 +3,11 @@ package br.com.caelum.leilao.testesgerais;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import org.junit.Test;
 import org.mockito.Answers;
@@ -17,6 +22,13 @@ class Bar{
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public String encrypt(String text) throws NoSuchAlgorithmException {
+		//simula criptografia, mas na verdade apenas retorna o MD5
+		MessageDigest m = MessageDigest.getInstance("MD5");
+		m.update(text.getBytes(), 0, text.length());
+		return new BigInteger(1,m.digest()).toString(16);
 	}
 }
 
@@ -59,6 +71,37 @@ public class TestesGerais {
 		assertEquals("Enzo", bar.getName());
 		assertEquals("Iris", bar.getName());
 		
+	}
+	
+	@Test
+	public void comoConfigurarOMockParaReceberQualquerString() throws NoSuchAlgorithmException {
+		
+//		System.out.println(new Bar().encrypt("Bruno"));
+//		System.out.println(new Bar().encrypt("Diciane"));
+//		System.out.println(new Bar().encrypt("Enzo"));
+//		System.out.println(new Bar().encrypt("Iris"));
+		
+		//Teste mockado
+		
+		Bar bar = mock(Bar.class);		
+		
+		when(bar.encrypt(anyString())). //O mock eh configurado para receber qualquer String
+			thenReturn("9b2b78033ecf0401a2feab5b4ba7462e", "c105c051a5bdda3c086702538ee3e535", 
+				"bca236048835bcf1f95e761e2e442393", "d25c7286a46dbb6eeecc5cfddacd5818");
+		
+		assertEquals("9b2b78033ecf0401a2feab5b4ba7462e", bar.encrypt("Bruno"));
+		assertEquals("c105c051a5bdda3c086702538ee3e535", bar.encrypt("Diciane"));
+		assertEquals("bca236048835bcf1f95e761e2e442393", bar.encrypt("Enzo"));
+		assertEquals("d25c7286a46dbb6eeecc5cfddacd5818", bar.encrypt("Iris"));
+		
+		//Teste chamando o metodo de verdade
+		
+		Bar bar2 = new Bar();
+		
+		assertEquals("9b2b78033ecf0401a2feab5b4ba7462e", bar2.encrypt("Bruno"));
+		assertEquals("c105c051a5bdda3c086702538ee3e535", bar2.encrypt("Diciane"));
+		assertEquals("bca236048835bcf1f95e761e2e442393", bar2.encrypt("Enzo"));
+		assertEquals("d25c7286a46dbb6eeecc5cfddacd5818", bar2.encrypt("Iris"));
 	}
 
 }
